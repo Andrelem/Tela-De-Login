1dd0916d6ae4edfed43b75872bb15b08504896e3
Andrelem <progameslepe@gmail.com>
2022-01-25 01:23:37 -0300

    '

--- telalogin/src/App.js
+++ telalogin/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login"> 
+          <form className="login-form">
+            <span className="login-title">Faça o Login</span>
+            <div className="wrap-input">
+              <input type="email"/>
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+            <div className="wrap-input">
+              <input type="password"/>
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+            <div className="container-login-form-btn">
+              <button className="login-btn">Login</button>
+            </div>
+            <div className="text-dowm">
+                <span className="txt1">Não possui conta?</span>
+
+                <a className="txt2" href="#">Crir conta...</a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
